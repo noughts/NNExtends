c9722b69400be9b82ea0cbf5ab49f6205db6b28b
koichi yamamoto <koichi@dividual.jp>
2015-02-24 15:58:18 +0900

    a

---
+++ Pod/Classes/NNButton.h
@@ -0,0 +1,13 @@
+//
+//  NNButton.h
+//  Pods
+//
+//  Created by noughts on 2015/02/24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNButton : UIButton
+
+@end
---
+++ Pod/Classes/NNButton.m
@@ -0,0 +1,21 @@
+//
+//  NNButton.m
+//  Pods
+//
+//  Created by noughts on 2015/02/24.
+//
+//
+
+#import "NNButton.h"
+
+@implementation NNButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
